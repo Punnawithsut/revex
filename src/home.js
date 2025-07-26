59f92740aaa4783b9927224ee87e932c258cfc24
Punnawithsut <punnawith05@gmail.com>
2025-07-26 21:26:46 +0700

    fix bugs

--- src/home.js
+++ src/home.js
@@ -1,5 +1,5 @@
 const get_start_button = document.getElementById("get_start_button");
 
 get_start_button.addEventListener("click", () => {
-    window.location.href = "CSV_creater.html";
+    window.location.href = "./src/CSV_creater.html";
 });
